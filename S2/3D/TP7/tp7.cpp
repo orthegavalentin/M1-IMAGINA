b5c01ed13420c03bdaf104d8bdccecc9ec89d114
Ooya <thibaut.castanie@gmail.com>
2015-04-18 11:57:44 +0200

    TP7 3D Q2
    à tester

--- S2/3D/TP7/tp7.cpp
+++ S2/3D/TP7/tp7.cpp
@@ -113,6 +113,76 @@ void drawSphere(int rayon, int nsides) {
 }
 
 void gauss(){
-	//dessiner les lignes de (0,0,0) à (x,y,z) de chaque point, plus une sphère.
+	std::ifstream fichier("triceratops.off");
+	std::string ligne;
+	int nbLignes=0;
+	int nbSommets=0;
+	int nbFaces=0;
+	int nbAretes=0;
+	double* tabSommets;
+	int* tabFaces;
+	double max = 10;
+	double min = -10;
+	double valRetour = 0;
+
+	while (std::getline(fichier, ligne)){
+		nbLignes++;
+		if (nbLignes == 1 && ligne=="OFF"){
+			//cout << "Fichier OFF détecté" << endl;
+		}
+		if (nbLignes == 2){
+			std::istringstream iss(ligne);
+			int nbS,nbF,nbA;
+			iss >> nbS >> nbF >> nbA;
+			nbSommets=nbS;
+			nbFaces=nbF;
+			nbAretes=nbA;
+			tabSommets = new double[nbSommets*3];
+			tabFaces = new int[nbFaces*3];
+		}
+
+
+		if (nbLignes>2 && nbLignes<=nbSommets+2){
+			std::istringstream iss(ligne);
+			double pt1, pt2, pt3;
+			iss >> pt1 >> pt2 >> pt3;
+			tabSommets[3*(nbLignes-3)]=pt1;
+			tabSommets[3*(nbLignes-3)+1]=pt2;
+			tabSommets[3*(nbLignes-3)+2]=pt3;
+		}
+		if (nbLignes>nbSommets+2){
+			std::istringstream iss(ligne);
+			int nb, p1, p2, p3;
+			iss >> nb >> p1 >> p2 >> p3;
+			tabFaces[3*(nbLignes-(nbSommets+3))]=p1;
+			tabFaces[3*(nbLignes-(nbSommets+3))+1]=p2;
+			tabFaces[3*(nbLignes-(nbSommets+3))+2]=p3;
+		}
+	}
+
+	for (int i = 0; i < nbSommets*3; i++){
+		tabSommets[i] *= 100;
+	}
+
+	glutwiresphere(8,16,16);
+
+	for (int i = 0; i < nbFaces*3; i+=3){
+
+		glBegin(GL_LINES);
+
+		glVertex3f(0,0,0);
+		glVertex3d(tabSommets[3*tabFaces[i]], tabSommets[3*tabFaces[i]+1] , tabSommets[3*tabFaces[i]+2]);
+		glVertex3f(0,0,0);
+		glVertex3d(tabSommets[3*tabFaces[i+1]], tabSommets[3*tabFaces[i+1]+1] , tabSommets[3*tabFaces[i+1]+2]);
+		glVertex3f(0,0,0);
+		glVertex3d(tabSommets[3*tabFaces[i+2]], tabSommets[3*tabFaces[i+2]+1] , tabSommets[3*tabFaces[i+2]+2]);
+
+
+		//glVertex3d(tabSommets[3*tabFaces[i]], tabSommets[3*tabFaces[i]+1] , tabSommets[3*tabFaces[i]+2]);
+		//glVertex3d(tabSommets[3*tabFaces[i+1]], tabSommets[3*tabFaces[i+1]+1] , tabSommets[3*tabFaces[i+1]+2]);
+		//glVertex3d(tabSommets[3*tabFaces[i+2]], tabSommets[3*tabFaces[i+2]+1] , tabSommets[3*tabFaces[i+2]+2]);
+
+		glEnd();
+	}
 	
 }
